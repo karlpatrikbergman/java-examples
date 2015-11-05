7df399d03cc3696b5c3cbebd3bc44f7fdc1e047a
karlpatrikbergman <patrik_bergman@yahoo.com>
2015-11-05 12:08:28 +0100

    Added unittest

--- src/main/java/se/patrikbergman/java/bigdecimal/BigDecimalApp.java
+++ src/main/java/se/patrikbergman/java/bigdecimal/BigDecimalApp.java
@@ -14,32 +14,19 @@ import java.math.RoundingMode;
  * 13522                I MIB value --> 13 522 MB/s
  */
 
-public class BigDecimalApp {
+class BigDecimalApp {
 
-    public static void main(String args[]) {
-        int speed = 1023;
-        System.out.println(
-                toDisplayString(speed)
-        );
-
-//        System.out.println(value);
-//        System.out.println(
-//            toIntegerInHundredsOfMegaBits(
-//                toBigDecimalWithTwoDecimals("10.23")
-//            ).intValueExact()
-//        );
+    BigDecimal toBigDecimalWithTwoDecimals(String stringValue) {
+        return BigDecimal
+                .valueOf(Double.valueOf(stringValue))
+                .setScale(2, RoundingMode.HALF_DOWN);
     }
 
-    private static BigDecimal toBigDecimalWithTwoDecimals(String textValue) {
-        Double doubleValue = Double.valueOf(textValue);
-        return BigDecimal.valueOf(doubleValue).setScale(2, RoundingMode.HALF_DOWN);
-    }
-
-    private static BigInteger toIntegerInHundredsOfMegaBits(BigDecimal value) {
+    BigInteger toIntegerInHundredsOfMegaBits(BigDecimal value) {
         return value.multiply(BigDecimal.valueOf(100)).toBigInteger();
     }
 
-    public static String toDisplayString(int speed) {
+    String toDisplayString(int speed) {
         return BigDecimal.valueOf(speed)
                 .divide(BigDecimal.valueOf(100))
                 .toString();
---
+++ src/test/java/se/patrikbergman/java/bigdecimal/BigDecimalAppTest.java
@@ -0,0 +1,38 @@
+package se.patrikbergman.java.bigdecimal;
+
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class BigDecimalAppTest {
+
+    BigDecimalApp bigDecimalApp = new BigDecimalApp();
+
+    @Test
+    public void toBigDecimalWithTwoDecimals() {
+        final String valueInMegabitsPerSecond = "10.23";
+        assertEquals(BigDecimal.valueOf(10.23), bigDecimalApp.toBigDecimalWithTwoDecimals(valueInMegabitsPerSecond));
+    }
+
+    @Test
+    public void toDisplayString() {
+        int intValue = 1023;
+        assertTrue("10.23".equals(bigDecimalApp.toDisplayString(intValue)));
+    }
+
+    @Test
+    public void toIntegerInHundredsOfMegaBits() {
+        assertEquals(1023, bigDecimalApp.toIntegerInHundredsOfMegaBits(BigDecimal.valueOf(10.23)).intValueExact());
+    }
+
+//        System.out.println(value);
+//        System.out.println(
+//            toIntegerInHundredsOfMegaBits(
+//                toBigDecimalWithTwoDecimals("10.23")
+//            ).intValueExact()
+//        );
+//    }
+}
