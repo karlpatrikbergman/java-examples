41f530dcd979de38ca2e5ef7f3cd4b30a6dfed96
karlpatrikbergman <patrik_bergman@yahoo.com>
2015-11-30 14:33:59 +0100

    Renews nested classes example

--- src/main/java/se/patrikbergman/java/classes/nested/OuterClassForBandMembers.java
+++ src/main/java/se/patrikbergman/java/classes/nested/OuterClassForBandMembers.java
@@ -1,40 +1,81 @@
 package se.patrikbergman.java.classes.nested;
 
-public class OuterClassForBandMembers {
+class OuterClassForBandMembers {
 
-	private final String tourMemory = "We had great burgers in Stockholm in 2002";
+    final String instanceTourMemory;
+    static final String STATIC_TOUR_MEMORY = "Static Tour Memory: I don't remember";
 
-	static final class StaticBandMember {
-		private final String name;
+    OuterClassForBandMembers(String instanceTourMemory) {
+        this.instanceTourMemory = instanceTourMemory;
+    }
 
-		public StaticBandMember(String name) {
-			this.name = name;
-		}
+    String getInstanceTourMemory() {
+        return instanceTourMemory;
+    }
 
-		public String getName() {
-			return name;
-		}
+    /**
+     * Nested classes that are declared static are called "static nested classes".
+     * As with class methods and variables, a static nested class is associated with its outer class.
+     * And like static class methods, a static nested class cannot refer directly to instance variables or
+     * methods defined in its enclosing class: it can use them only through an object reference.
+     */
+    static final class StaticNestedClassBandMember {
+        private final String name;
 
-		public String getTourMemory() {
-			//Compile error
-//			return tourMemory;
-			return "I don't remember";
-		}
-	}
+        public StaticNestedClassBandMember(String name) {
+            this.name = name;
+        }
 
-	public final class NestedBandMember {
-		private final String name;
+        public String getName() {
+            return name;
+        }
 
-		public NestedBandMember(String name) {
-			this.name = name;
-		}
+        /**
+         * See StaticNestedClassBandMember description
+         */
+        public String getTourMemory() {
+        //Compile error
+        //return tourMemory;
+          return STATIC_TOUR_MEMORY;
+        }
+    }
 
-		public String getName() {
-			return name;
-		}
+    /**
+     * Non-static nested classes are called "inner classes"
+     * Non-static nested classes (inner classes) have access to other members of the enclosing class, even if they
+     * are declared private (in this example TOUR_MEMORY)
+     *
+     * As with instance methods and variables, an inner class is associated with an instance of its enclosing class
+     * and has direct access to that object's methods and fields.
+     *
+     * Also, because an inner class is associated with an instance, it cannot define any static members itself.
+     *
+     * Objects that are instances of an inner class exist within an instance of the outer class.
+     *
+     * An instance of InnerClass can exist only within an instance of OuterClass and has direct access to the methods
+     * and fields of its enclosing instance.
+     *
+     * To instantiate an inner class, you must first instantiate the outer class. Then, create the inner object within
+     * the outer object with this syntax:
+     * OuterClass.InnerClass innerObject = outerObject.new InnerClass();
+     *
+     */
+    final class InnerClassBandMember {
+        //Does not compile, "inner classes cannot have static declarations"
+        //static String myString = "";
 
-		public String getTourMemory() {
-			return tourMemory;
-		}
-	}
+        private final String name;
+
+        public InnerClassBandMember(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getTourMemory() {
+            return getInstanceTourMemory();
+        }
+    }
 }
--- src/test/java/se/patrikbergman/java/classes/nested/NestedClassesTest.java
+++ src/test/java/se/patrikbergman/java/classes/nested/NestedClassesTest.java
@@ -3,20 +3,26 @@ package se.patrikbergman.java.classes.nested;
 import org.junit.Test;
 import se.patrikbergman.java.utility.JsonString;
 
-import java.io.IOException;
-
-import static se.patrikbergman.java.classes.nested.OuterClassForBandMembers.StaticBandMember;
+import static org.junit.Assert.assertEquals;
+import static se.patrikbergman.java.classes.nested.OuterClassForBandMembers.STATIC_TOUR_MEMORY;
 
 public class NestedClassesTest {
 
+
+
 	@Test
-	public void testNestedClasses() throws IOException {
-		OuterClassForBandMembers outerClassForBandMembers = new OuterClassForBandMembers();
-		OuterClassForBandMembers.NestedBandMember nestedBandMember = outerClassForBandMembers. new NestedBandMember("William Goldsmith");
-		StaticBandMember staticBandMember = new StaticBandMember("Dave Grohl");
-
-		System.out.println(new JsonString(nestedBandMember));
-		System.out.println(new JsonString(staticBandMember));
-
+	public void testStaticNestedClassBandMember() {
+		OuterClassForBandMembers.StaticNestedClassBandMember staticNestedClassBandMember = new OuterClassForBandMembers.StaticNestedClassBandMember("Dave Grohl");
+		assertEquals(STATIC_TOUR_MEMORY, staticNestedClassBandMember.getTourMemory());
+        System.out.println(new JsonString(staticNestedClassBandMember));
 	}
+
+    @Test
+    public void testInnerClassBandMember() {
+        final String instanceTourMemory = "Instance Tour Memory: I am We had great burgers in Stockholm in 2002";
+        OuterClassForBandMembers outerClassForBandMembers = new OuterClassForBandMembers(instanceTourMemory);
+        OuterClassForBandMembers.InnerClassBandMember innerClassBandMember = outerClassForBandMembers.new InnerClassBandMember("William Goldsmith");
+        assertEquals(instanceTourMemory, innerClassBandMember.getTourMemory());
+        System.out.println(new JsonString(innerClassBandMember));
+    }
 }
